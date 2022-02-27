7cbb2446585f5dd4aba4081fae8fecf07fa1a7f4
Do Tung <54771496+dt-japanscript@users.noreply.github.com>
2022-02-27 12:08:29 +0900

    Add typing animation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/** ============= typing animation ============= */
+var typed = new Typed(".typing", {
+    strings: ["CAD Engineer", "CAE Engineer", "Python Junior"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
